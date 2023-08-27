e15a90741ea99543745e0e5df79d369bfe346c7a
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-27 22:02:17 +0545

    Ok

--- app/(root)/components/SideBar.tsx
+++ app/(root)/components/SideBar.tsx
@@ -1,7 +1,13 @@
+"use client";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import { AiOutlineDashboard } from "react-icons/ai";
+import { BiNotepad } from "react-icons/bi";
+import Link from "next/link";
 
 export default function SideBar() {
+  const pathname = usePathname();
+
   return (
     <div className="flex flex-col w-[22%] items-center gap-8 bg-dark-800 py-10 fixed min-h-[100vh]">
       <div className="flex flex-col items-center">
@@ -17,23 +23,45 @@ export default function SideBar() {
           prajwolneupane68@gmail.com
         </h4>
       </div>
+      <Link
+        href={"/dashboard"}
+        className="flex items-center gap-5 w-full text-text-400 h-[50px]"
+      >
+        <div
+          className={`${
+            pathname.includes("/dashboard") ? "bg-brand-700" : "bg-transparent"
+          } w-[3px] h-full`}
+        ></div>
+        <AiOutlineDashboard className="text-m" />
+        <h3 className="font-r text-3xs">Dashboard</h3>
+      </Link>
+      <Link
+        href={"/manage"}
+        className="flex items-center gap-5 w-full text-text-400 h-[50px]"
+      >
+        <div
+          className={`${
+            pathname.includes("/manage") ? "bg-brand-700" : "bg-transparent"
+          } w-[3px] h-full`}
+        ></div>
+        <BiNotepad className="text-m" />
+        <h3 className="font-r text-3xs">Manage</h3>
+      </Link>
       <div className="flex items-center gap-5 w-full text-text-400 h-[50px]">
-        <div className="bg-brand-700 w-[3px] h-full"></div>
+        <div
+          className={`${
+            pathname.includes("/ok") ? "bg-brand-700" : "bg-transparent"
+          } w-[3px] h-full`}
+        ></div>
         <AiOutlineDashboard className="text-m" />
         <h3 className="font-r text-3xs">Dashboard</h3>
       </div>
       <div className="flex items-center gap-5 w-full text-text-400 h-[50px]">
-        <div className="bg-transparent w-[3px] h-full"></div>
-        <AiOutlineDashboard className="text-m" />
-        <h3 className="font-r text-3xs">Dashboard</h3>
-      </div>
-      <div className="flex items-center gap-5 w-full text-text-400 h-[50px]">
-        <div className="bg-transparent w-[3px] h-full"></div>
-        <AiOutlineDashboard className="text-m" />
-        <h3 className="font-r text-3xs">Dashboard</h3>
-      </div>
-      <div className="flex items-center gap-5 w-full text-text-400 h-[50px]">
-        <div className="bg-transparent w-[3px] h-full"></div>
+        <div
+          className={`${
+            pathname.includes("/kl") ? "bg-brand-700" : "bg-transparent"
+          } w-[3px] h-full`}
+        ></div>
         <AiOutlineDashboard className="text-m" />
         <h3 className="font-r text-3xs">Dashboard</h3>
       </div>
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes:['/','/api/webhook/clerk','/dashboard'] ,
+    publicRoutes:['/','/api/webhook/clerk','/dashboard','/manage'] ,
     ignoredRoutes:['/api/webhook/clerk']
 });
 
